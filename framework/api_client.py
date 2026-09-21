a05a3a03eee9cdf9eabed50d216900dd8c00d913
jasminepal <jasmine4pal@gmail.com>
2026-09-21 19:58:22 +0530

    API client robustness added

--- framework/api_client.py
+++ framework/api_client.py
@@ -1,19 +1,23 @@
 import requests
-from framework.config import BASE_URL
+from framework.config import BASE_URL, REQUEST_TIMEOUT
 
 class APIClient:
+    
+    def _request(self, method, endpoint, data = None):
+        return requests.request(method, f"{BASE_URL}{endpoint}", json=data, timeout=REQUEST_TIMEOUT)
+        
     def get(self, endpoint):
-        return requests.get(f"{BASE_URL}{endpoint}")
+        return self._request("GET", endpoint)
     
     def post(self, endpoint, request_body):
-        return requests.post(f"{BASE_URL}{endpoint}", json = request_body)
+        return self._request("POST", endpoint, request_body)
     
     def put(self, endpoint, request_body):
-        return requests.put(f"{BASE_URL}{endpoint}", json = request_body)
+        return self._request("PUT", endpoint, request_body)
     
     def patch(self, endpoint, request_body):
-        return requests.patch(f"{BASE_URL}{endpoint}", json = request_body)
+        return self._request("PATCH", endpoint, request_body)
         
     def delete(self, endpoint):
-        return requests.delete(f"{BASE_URL}{endpoint}")
+        return self._request("DELETE", endpoint)
     
